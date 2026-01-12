4bb15433874a93d86ea33707cf83162945dad32f
RobertoScarci <robertoscarci.02@gmail.com>
2026-01-12 11:42:58 +0100

    feat: export new database and seeding services
    - Export DatabaseService and CitySeederService for use across app

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -2,6 +2,8 @@ export * from './storage.service';
 export * from './user.service';
 export * from './city.service';
 export * from './personalization.service';
+export * from './database.service';
+export * from './city-seeder.service';
 
 // External API Services
 export * from './api/weather.service';
